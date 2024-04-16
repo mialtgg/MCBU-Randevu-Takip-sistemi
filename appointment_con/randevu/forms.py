a664932a87bef97288393b88ad397f157a1ab16e
mialtgg <200316021@ogr.cbu.edu.tr>
2024-04-16 16:41:12 +0300

    charts updated

--- appointment_con/randevu/forms.py
+++ appointment_con/randevu/forms.py
@@ -7,7 +7,7 @@ from .models import Event
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['customer_name', 'description', 'start_time', 'end_time','joining_date','status','institution_name','contact','status_description','type','appointment_type']
 class YourForm(forms.Form):
     admin_add_name = forms.ModelChoiceField(queryset=User.objects.all())
     
